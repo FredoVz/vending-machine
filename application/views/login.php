1fb0a4e858f14a6079103b13c96dd3386deb4615
FredoVz <dm.fredo018@gmail.com>
2024-12-21 09:09:03 +0700

    first commit

--- application/views/login.php
+++ application/views/login.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Vending Machine</title>
+    <title>Vending Machine - Login</title>
 
     <style>
         body {
